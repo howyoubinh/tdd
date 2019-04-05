dfd9d5e4d4694045fac829ff295dbce52383f782
Binh Nguyen <howyoubinh1@gmail.com>
2019-04-04 21:07:46 -0700

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
-
-# Create your tests here.
 
 class HomePageTest(TestCase):
 
